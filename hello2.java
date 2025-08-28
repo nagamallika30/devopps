006613623865dcebd13eaca4d9950022ef6f67e3
nandhu <nandhumaripelly1@gmail.com>
2025-08-28 14:56:51 +0530

    one

---
+++ hello2.java
@@ -0,0 +1,12 @@
+public class hello2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+System.out.println("welcome to DeVops ");
+System.out.println("welcome to ml lab ");
+
+System.out.println("i love java programming ");
+
+
+
+    }
+}
